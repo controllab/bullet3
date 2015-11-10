b7447a50588aa81293e00778c99bcd7ed0077d2a
Marcel Groothuis <margro.xbmc@gmail.com>
2015-11-10 14:19:30 +0100

    Add batch file for VS2013 project generation

---
+++ build3/generate_vs2013.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set cmake_version=3.0
+set TOOLVERSION=msvc12
+
+IF EXIST "%xxsim_toolchain%\addToolchain32ToPath.bat" (
+	CALL "%xxsim_toolchain%\addToolchain32ToPath.bat"
+)
+
+set curdir = %CD%
+
+mkdir vs2013 2> NUL
+
+cd vs2013
+
+cmake ..\..\ -G "Visual Studio 12" -DUSE_DOUBLE_PRECISION=ON -DUSE_MSVC_FAST_FLOATINGPOINT=OFF -DCMAKE_INSTALL_PREFIX=E:\temp\kogel
+
+cd %curdir%
